84e65715fd7a3e30e644e6d52f5c1270e91adfdb
yoiner castillo <jhoicas@gmail.com>
2026-09-16 23:32:27 -0500

    fix(tenant): corregir cierre de etiqueta JSX en PlanDesarrolloTab

--- starter/frontend/src/components/Tenant/MGA/PlanDesarrolloTab.tsx
+++ starter/frontend/src/components/Tenant/MGA/PlanDesarrolloTab.tsx
@@ -193,7 +193,6 @@ export default function PlanDesarrolloTab({ project }: { project: Project }) {
                 </tbody>
               </table>
             </div>
-            </div>
             <button 
               type="button" 
               onClick={() => setIsPndModalOpen(true)} 
